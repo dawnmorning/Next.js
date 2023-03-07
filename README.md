cf097fdc9e2196d339653b9365745aa255adc383
kimjonghyeok <notify9637@naver.com>
2023-03-08 08:47:23 +0900

    Next.js pages 공부

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Next.js
+
+## Next.js 와 JavaScript를 먼저 공부한 뒤 TypeScript를 병합해보려고 하는 공부 레포지토리입니다.
---
+++ my-fist-app/components/NavBar.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav>
+      <Link href="/">
+        <div>Home</div>
+      </Link>
+      <Link href="/about">
+        <div>About</div>
+      </Link>
+    </nav>
+  );
+}
